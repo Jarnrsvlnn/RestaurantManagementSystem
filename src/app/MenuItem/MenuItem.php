04111c91f97f1e3acb97f6bf4ca234d7b1b58c50
Jarnrsvlnn <173128640+Jarnrsvlnn@users.noreply.github.com>
2025-09-30 16:19:41 +0800

    Initial commit

---
+++ src/app/MenuItem/MenuItem.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MenuItem;
+
+class MenuItem {
+    public function __construct(
+        private string $name,
+        private float $price, 
+        private int $stock
+    )
+    {}
+
+    public function orderItem() {
+        if ($this->stock > 0) {
+            $this->stock--;
+        }
+        else {
+            echo 'No Stock!';
+        }
+    }
+
+    public function restockItem() {
+        $this->stock++;
+    }
+
+    public function getInfo() {
+        
+    }
+}
